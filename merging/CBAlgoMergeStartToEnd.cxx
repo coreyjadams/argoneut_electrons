d0e36f8da3f77d194ebf3145514d8342708c3bf0
Nicole Pereira <npereira@uboonegpvm01.fnal.gov>
2015-06-25 13:47:19 -0500

    vertex cluster fix...maybe

--- merging/CBAlgoMergeStartToEnd.cxx
+++ merging/CBAlgoMergeStartToEnd.cxx
@@ -2,6 +2,7 @@
 #define RECOTOOL_CBALGOMERGESTARTTOEND_CXX
 
 #include "CBAlgoMergeStartToEnd.h"
+#include "math.h"
 #include "../../RecoTool/ClusterRecoUtil/ClusterParamsAlg.h"
 #include <iostream>
 #include "CMTool/CMToolBase/CBoolAlgoBase.h"
@@ -28,27 +29,31 @@ namespace cmtool {
     // Area() is type double, not Polygon2D 
     //double polyArea = overlapPoly.PolyObject.Area();
 
-    float maxDist = 2 ; //semicolon; Can't use "int" for non-integer (like 0.5).
+    float maxDist = 1 ; //semicolon; Can't use "int" for non-integer (like 0.5).
     float actDist1 = pow(pow((cluster1.GetParams().start_point.w - cluster2.GetParams().end_point.w), 2) + pow((cluster1.GetParams().start_point.t - cluster2.GetParams().end_point.t), 2), 0.5); //extra parentheses
     float actDist2 = pow(pow((cluster1.GetParams().end_point.w - cluster2.GetParams().start_point.w), 2) + pow((cluster1.GetParams().end_point.t - cluster2.GetParams().start_point.t), 2), 0.5); //extra parentheses
     //new lines below
     float actDist3 = pow(pow((cluster1.GetParams().start_point.w - cluster2.GetParams().start_point.w), 2) + pow((cluster1.GetParams().start_point.t - cluster2.GetParams().start_point.t), 2), 0.5); //extra parentheses
     //float actDist4 = pow(pow((cluster1.GetParams().end_point.w - cluster2.GetParams().end_point.w), 2) + pow((cluster1.GetParams().end_point.t - cluster2.GetParams().end_point.t), 2), 0.5); //extra parentheses
-
+    //auto avgAngle1 = (cluster1.GetParams().opening_angle + cluster1.GetParams().closing_angle) / 2 ;
+    //auto avgAngle2 = (cluster2.GetParams().opening_angle + cluster2.GetParams().closing_angle) / 2 ;
+    float rmsDist = pow(pow(abs(cluster1.GetParams().rms_x - cluster2.GetParams().rms_x), 2) + pow(abs(cluster1.GetParams().rms_y - cluster2.GetParams().rms_y), 2), 0.5);
 
     if (cluster1.GetNHits() < 3 && cluster1.GetNHits() < 3)
       return false;
     else
-      if ( overlap && ( actDist1 < maxDist || actDist2 < maxDist ) && (cluster1.GetParams().angle_2d <= cluster2.GetParams().angle_2d + 0.05 || cluster1.GetParams().angle_2d >= cluster2.GetParams().angle_2d - 0.05)) 
-        return true;
-      else if (actDist1 < maxDist || actDist2 < maxDist)      //if the clusters' start and end points are within a certain distance of each other, return true to merge
-        return true;
+      if (cluster1.GetParams().direction != cluster2.GetParams().direction)
+        return false;
       else if ((actDist1 < 4 || actDist2 < 4) && (cluster1.GetParams().angle_2d <= cluster2.GetParams().angle_2d + 0.0125 || cluster1.GetParams().angle_2d >= cluster2.GetParams().angle_2d - 0.0125))
         return true;
-      else if (actDist3 < maxDist)
-        return false;
-      else if (overlapPoly.Area() <= 0.5)
+      else if (overlapPoly.Area() < 4)
        return false;
+      else if (actDist3 < 0.0125)
+        return false;
+      else if ( overlap && ( actDist1 < maxDist || actDist2 < maxDist ) && (cluster1.GetParams().angle_2d <= cluster2.GetParams().angle_2d + 0.0125 || cluster1.GetParams().angle_2d >= cluster2.GetParams().angle_2d - 0.0125) && overlapPoly.Area() > 4)
+	return true;
+      else if (rmsDist > maxDist)
+	return false;
       else 
        return false;
     
@@ -57,3 +62,4 @@ namespace cmtool {
 }
 
 #endif
+
