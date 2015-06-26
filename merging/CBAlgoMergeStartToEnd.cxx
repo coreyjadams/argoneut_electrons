4337c634557d2501c8a184a84c4cb1564518a46f
Nicole Pereira <npereira@uboonegpvm01.fnal.gov>
2015-06-26 11:33:24 -0500

    errors in slope function

--- merging/CBAlgoMergeStartToEnd.cxx
+++ merging/CBAlgoMergeStartToEnd.cxx
@@ -10,6 +10,32 @@
 
 namespace cmtool {
 
+float best_slope () {
+
+  //const ::cluster::ClusterParamsAlg &cluster
+
+  //auto ev_cluster = cluster ;
+  double event_index = 0;
+  auto ev_cluster = storage->get_data<event_index>("cccluster");
+  auto ev_hit = storage->get_data<event_index>("cccluster");
+  auto ass_info = ev_cluster.association(ev_hit.id());
+
+  float diff_x_sq = 0;
+  float diff_x_by_diff_y = 0;
+  float mean_x = cluster.GetParams().mean_x;
+  float mean_y = cluster.GetParams().mean_y;
+  float cluster_slope;
+
+  for(auto const& hit_indices : ass_info) {
+    for(auto const& hit_index : hit_indices) {
+      diff_x_by_diff_y += (ev_hit[hit_index].X() - mean_x) * (ev_hit[hit_index].Y() - mean_y) ;
+      diff_x_sq += pow((ev_hit[hit_index].X() - mean_x), 2) ;
+      event_index++ ;
+    }
+    cluster_slope = diff_x_by_diff_y / diff_x_sq ;
+  }
+  return cluster_slope ;
+}
   
   CBAlgoMergeStartToEnd::CBAlgoMergeStartToEnd() : CBoolAlgoBase()
   {
@@ -21,12 +47,10 @@ namespace cmtool {
       const ::cluster::ClusterParamsAlg &cluster2)
   {
 
-    //Use auto when you're not sure what the type is-- PolyOverlap is a boolean, not Polygon2D
     auto overlap = cluster2.GetParams().PolyObject.PolyOverlap(cluster1.GetParams().PolyObject ) ;
     auto overlapPoly = Polygon2D(cluster1.GetParams().PolyObject, cluster2.GetParams().PolyObject);
     
 
-    // Area() is type double, not Polygon2D 
     //double polyArea = overlapPoly.PolyObject.Area();
 
     float maxDist = 1 ; //semicolon; Can't use "int" for non-integer (like 0.5).
@@ -38,28 +62,36 @@ namespace cmtool {
     //auto avgAngle1 = (cluster1.GetParams().opening_angle + cluster1.GetParams().closing_angle) / 2 ;
     //auto avgAngle2 = (cluster2.GetParams().opening_angle + cluster2.GetParams().closing_angle) / 2 ;
     float rmsDist = pow(pow(abs(cluster1.GetParams().rms_x - cluster2.GetParams().rms_x), 2) + pow(abs(cluster1.GetParams().rms_y - cluster2.GetParams().rms_y), 2), 0.5);
+    float slope1 = cluster1.best_slope() ;
+    float slope2 = cluster2.best_slope() ;
 
     if (cluster1.GetNHits() < 3 && cluster1.GetNHits() < 3)
       return false;
     else
       if (cluster1.GetParams().direction != cluster2.GetParams().direction)
         return false;
-      else if (((actDist1 < 4 && actDist1 > 1) || (actDist2 < 4 && actDist2 > 1)) && (cluster1.GetParams().angle_2d <= cluster2.GetParams().angle_2d + 0.0125 || cluster1.GetParams().angle_2d >= cluster2.GetParams().angle_2d - 0.0125))
+      else if (slope1 >= slope2 - 0.125 || slope1 <= slope2 + 0.125)
+        return true;
+      else if (((actDist1 < 4 && actDist1 > 1) || (actDist2 < 4 && actDist2 > 1)) && ((slope1 >= slope2 - 0.125 || slope1 <= slope2 + 0.125)))
         return true;
       else if (overlapPoly.Area() < 4)
        return false;
       else if (actDist3 < 0.0125)
         return false;
-      else if ( overlap && ((actDist1 < 4 && actDist1 > 1) || (actDist2 < 4 && actDist2 > 1)) && (cluster1.GetParams().angle_2d <= cluster2.GetParams().angle_2d + 0.0125 || cluster1.GetParams().angle_2d >= cluster2.GetParams().angle_2d - 0.0125) && overlapPoly.Area() > 4)
-	return true;
+      else if ( overlap && ((actDist1 < 4 && actDist1 > 1) || (actDist2 < 4 && actDist2 > 1)) && (slope1 >= slope2 - 0.125 || slope1 <= slope2 + 0.125) && overlapPoly.Area() > 4)
+        return true;
       else if (rmsDist > maxDist)
-	return false;
+       return false;
       else 
        return false;
     
     
   }
 }
+
+
+
+
 
 #endif
 
