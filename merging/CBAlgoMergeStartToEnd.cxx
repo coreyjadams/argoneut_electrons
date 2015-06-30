44c45a323fa9c3f887452af53c883e4f97f9b30c
Nicole Pereira <npereira@uboonegpvm01.fnal.gov>
2015-06-30 11:13:34 -0500

    errors

--- merging/CBAlgoMergeStartToEnd.cxx
+++ merging/CBAlgoMergeStartToEnd.cxx
@@ -10,11 +10,10 @@
 
 namespace cmtool {
 
+//defining best_slope
 float CBAlgoMergeStartToEnd::best_slope (const ::cluster::ClusterParamsAlg & cluster) {
 
-    auto hit_v = cluster.GetHitVector() ;
-
-//
+  auto hit_v = cluster.GetHitVector() ;
   float diff_x_sq = 0;
   float diff_x_by_diff_y = 0;
   float mean_x = cluster.GetParams().mean_x;
@@ -22,15 +21,58 @@ float CBAlgoMergeStartToEnd::best_slope (const ::cluster::ClusterParamsAlg & clu
   float cluster_slope = 0;
 
     for(auto const& hit_index : hit_v) {
-      diff_x_by_diff_y += (hit_index.w - mean_x) * (hit_index.t - mean_y) ;
-      diff_x_sq += pow((hit_index.w - mean_x), 2) ;
+      if (hit_index.charge > 0.10 * cluster.GetParams().sum_charge)
+      	diff_x_by_diff_y += (hit_index.w - mean_x) * (hit_index.t - mean_y) ;
+      	diff_x_sq += pow((hit_index.w - mean_x), 2) ;
 
     }
+    //redefine cluster_slope here
     cluster_slope = diff_x_by_diff_y / diff_x_sq ;
 
   return cluster_slope ;
 }
+
+//defining getShortestDist
+float CBAlgoMergeStartToEnd::getShortestDist(
+      const ::cluster::ClusterParamsAlg &cluster1,
+      const ::cluster::ClusterParamsAlg &cluster2)
+  {
+
+
+    float minDist(9999.0);
+    for (auto & hit : cluster2.GetHitVector()){
+      for (auto & otherHit : cluster1.GetHitVector()){
+        float thisDist = sqrt(pow(hit.w - otherHit.w,2 ) + pow(hit.t - otherHit.t, 2));
+        if ( thisDist < minDist){
+          minDist = thisDist;
+        }
+      }
+    }
+    return minDist;
+  }
+
+  //defining touching
+  bool CBAlgoMergeStartToEnd::touching (const ::cluster::ClusterParamsAlg & cluster1, const ::cluster::ClusterParamsAlg & cluster2) {
+
+  auto hit_v_1 = cluster1.GetHitVector() ;
+  auto hit_v_2 = cluster2.GetHitVector() ;
+  bool touch = false;  
+
+    for(auto const& hit_index_1 : hit_v_1) {
+      for(auto const& hit_index_2 : hit_v_2) {
+        if (pow((pow((hit_index_1.w - hit_index_2.w), 2) + pow((hit_index_1.t - hit_index_2.t), 2)), 0.5) >= 0.45 && hit_index_1.w > hit_index_2.w && hit_index_1.t > hit_index_2.t)
+          touch = true;
+	else if ((hit_index_1.w == hit_index_2.w || hit_index_1.t == hit_index_2.t) && (pow((pow((hit_index_1.w - hit_index_2.w), 2) + pow((hit_index_1.t - hit_index_2.t), 2)), 0.5) < 0.7))
+          touch = true;
+	else
+          touch = false;
+      }
+    }
+    return touch;
+}
   
+
+  //main 
   CBAlgoMergeStartToEnd::CBAlgoMergeStartToEnd() : CBoolAlgoBase()
   {
   }
@@ -41,41 +83,67 @@ float CBAlgoMergeStartToEnd::best_slope (const ::cluster::ClusterParamsAlg & clu
       const ::cluster::ClusterParamsAlg &cluster2)
   {
 
-    auto overlap = cluster2.GetParams().PolyObject.PolyOverlap(cluster1.GetParams().PolyObject ) ;
-    auto overlapPoly = Polygon2D(cluster1.GetParams().PolyObject, cluster2.GetParams().PolyObject);
+    ::cluster::ClusterParamsAlg poly1 = cluster1;
+    ::cluster::ClusterParamsAlg poly2 = cluster2;
+    poly1.FillPolygon(1.);
+    poly2.FillPolygon(1.);
+    auto overlap = poly1.GetParams().PolyObject.PolyOverlap(poly2.GetParams().PolyObject);
+    auto overlapPoly = Polygon2D(poly1.GetParams().PolyObject,poly2.GetParams().PolyObject);
+    //auto overlap = cluster2.GetParams().PolyObject.PolyOverlap(cluster1.GetParams().PolyObject ) ;
+    //auto overlapPoly = Polygon2D(cluster1.GetParams().PolyObject, cluster2.GetParams().PolyObject);
     
 
-    //double polyArea = overlapPoly.PolyObject.Area();
-
-    float maxDist = 1 ; //semicolon; Can't use "int" for non-integer (like 0.5).
-    float actDist1 = pow(pow((cluster1.GetParams().start_point.w - cluster2.GetParams().end_point.w), 2) + pow((cluster1.GetParams().start_point.t - cluster2.GetParams().end_point.t), 2), 0.5); //extra parentheses
-    float actDist2 = pow(pow((cluster1.GetParams().end_point.w - cluster2.GetParams().start_point.w), 2) + pow((cluster1.GetParams().end_point.t - cluster2.GetParams().start_point.t), 2), 0.5); //extra parentheses
-    //new lines below
-    float actDist3 = pow(pow((cluster1.GetParams().start_point.w - cluster2.GetParams().start_point.w), 2) + pow((cluster1.GetParams().start_point.t - cluster2.GetParams().start_point.t), 2), 0.5); //extra parentheses
-    //float actDist4 = pow(pow((cluster1.GetParams().end_point.w - cluster2.GetParams().end_point.w), 2) + pow((cluster1.GetParams().end_point.t - cluster2.GetParams().end_point.t), 2), 0.5); //extra parentheses
-    //auto avgAngle1 = (cluster1.GetParams().opening_angle + cluster1.GetParams().closing_angle) / 2 ;
-    //auto avgAngle2 = (cluster2.GetParams().opening_angle + cluster2.GetParams().closing_angle) / 2 ;
+    float maxDist = 1 ;
+    float _max_distance = 0.125; 
+    float actDist1 = pow(pow((cluster1.GetParams().start_point.w - cluster2.GetParams().end_point.w), 2) + pow((cluster1.GetParams().start_point.t - cluster2.GetParams().end_point.t), 2), 0.5); 
+    float actDist2 = pow(pow((cluster1.GetParams().end_point.w - cluster2.GetParams().start_point.w), 2) + pow((cluster1.GetParams().end_point.t - cluster2.GetParams().start_point.t), 2), 0.5); 
+    float actDist3 = pow(pow((cluster1.GetParams().start_point.w - cluster2.GetParams().start_point.w), 2) + pow((cluster1.GetParams().start_point.t - cluster2.GetParams().start_point.t), 2), 0.5); 
+    float actDist4 = pow(pow((cluster1.GetParams().end_point.w - cluster2.GetParams().end_point.w), 2) + pow((cluster1.GetParams().end_point.t - cluster2.GetParams().end_point.t), 2), 0.5); 
     float rmsDist = pow(pow(abs(cluster1.GetParams().rms_x - cluster2.GetParams().rms_x), 2) + pow(abs(cluster1.GetParams().rms_y - cluster2.GetParams().rms_y), 2), 0.5);
     float slope1 = best_slope(cluster1) ;
     float slope2 = best_slope(cluster2) ;
 
-    if (cluster1.GetNHits() < 3 && cluster1.GetNHits() < 3)
+ 
+
+    if (cluster1.GetNHits() < 3 && cluster2.GetNHits() < 3)
       return false;
     else
       if (cluster1.GetParams().direction != cluster2.GetParams().direction)
         return false;
-      //else if (slope1 >= slope2 - 3 || slope1 <= slope2 + 3)
-        //return false;
-      //else if (overlapPoly.Area() < 4)
-       //return false;
-      else if ( overlap && ((actDist1 < 4 && actDist1 /*> 1*/) || (actDist2 < 4 && actDist2/* > 1*/)) && (slope1 >= slope2 - 0.25 || slope1 <= slope2 + 0.25) && overlapPoly.Area() > 4)
-       return true;
-      else if (((actDist1 < 4 && actDist1 /*> 1*/) || (actDist2 < 4 && actDist2 /*> 1*/)) && ((slope1 >= slope2 - 0.25 || slope1 <= slope2 + 0.25)))
-        return true;
-      else if (actDist3 < 0.05)
-        return false;
-      else if (rmsDist > maxDist)
+      if (getShortestDist(cluster1, cluster2) > 1)
+	return false;
+      if (slope1 > slope2 + 0.25 || slope1 < slope2 - 0.25)
+	return false;
+      else if (overlap && actDist1 < actDist3 && (overlapPoly.Area() > 1) /*&& slope1 >= slope2 - 0.125 && slope1 <= slope2 + 0.125*/)
+	if (touching(cluster1, cluster2) == true && actDist1 > actDist3)
+	  return false;
+	else
+	  return true;
+      else if ((overlapPoly.Area() > 0.2 * poly1.GetParams().PolyObject.Area() || overlapPoly.Area() > 0.2 * poly2.GetParams().PolyObject.Area()))
+	if ((slope1 >= slope2 - 0.00125 && slope1 <= slope2 + 0.00125))
+	  return false;
+	else
+	  return true;
+      else if ((overlapPoly.Area() > 0.6 * poly1.GetParams().PolyObject.Area() || overlapPoly.Area() > 0.6 * poly2.GetParams().PolyObject.Area()))
+	  return true;
+      else if (overlapPoly.PointInside(cluster1.GetParams().start_point) == true)
+	return true;
+      else if ((slope1 >= slope2 - 0.0125 && slope1 <= slope2 + 0.0125) && touching(cluster1, cluster2) == true && actDist1 > actDist3)
+	return true;
+      else if (actDist1 < 2 && (slope1 >= slope2 - 0.25 && slope1 <= slope2 + 0.25))
+	return true;
+      else if (getShortestDist(cluster1, cluster2) < _max_distance)
+        if (actDist3 > actDist1 || actDist3 > actDist2 || actDist4 > actDist1 || actDist4 > actDist2)
+          return false;
+        else
+          return true;
+      //try eliminating this line
+      else if (rmsDist >= maxDist)
        return false;
+      else if (actDist3 < 0.5)
+        return false;     
+      else if (touching(cluster1, cluster2) == true && ((slope1 >= slope2 - 0.75 || slope1 <= slope2 + 0.75)) && (actDist1 < rmsDist + 2 && actDist1 > rmsDist - 2))
+        return true;
       else 
        return false;
     
