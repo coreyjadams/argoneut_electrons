83e9c43890294901a51f5c11392e46d12785e513
Corey Adams <corey.adams@yale.edu>
2016-03-16 14:35:26 -0500

    Script to make dE/dx plots.

--- analysis/dEdxPlotter.py
+++ analysis/dEdxPlotter.py
@@ -1,96 +1,78 @@
 import ROOT
 import larlite
+import math
 
-from ROOT import larlite
+from ROOT import larlite, larutil
+from ROOT import TTree
 
 # use matplotlib to make these plots
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+import pandas as pd
+from root_numpy import root2array
 
 
-# Loop over the events and read out the showers.
-
-_mgr = larlite.storage_manager()
-
-# Here are the input files
-files = ["/data_linux/dedx_files/anu_electrons_reconstructed.root",
-         "/data_linux/dedx_files/anu_photons_reconstructed.root",
-         "/data_linux/dedx_files/nu_electrons_reconstructed.root",
-         "/data_linux/dedx_files/nu_photons_reconstructed.root"]
+Q_AREA =  [0.8152236862020928, 0.8078780681936417]
+Q_AMP =   [9.207991314407435, 18.562195730195935]
+E_AREA =  [185.00584273133333, 44.90267652367623]
+E_AMP =   [184.9556285310639, 45.1669419567248]
 
 
+def boxCorrection(dqdx_v):
+    result = []
+    for x in dqdx_v:
+        result.append(larutil.LArProperties.GetME().ModBoxCorrection(x))
+    return result
 
-electron_data = []
-photon_data = []
-
-electron_data_q = []
-photon_data_q = []
-
-electrons = False
-photons = False
-
-for f in files:
-  _mgr.reset()
-  _mgr.set_io_mode(larlite.storage_manager.kREAD)
-  _mgr.add_in_filename(f)
-  _mgr.open()
-
-  if "electrons" in f:
-    electrons = True
-    photons = False
-  if "photons" in f:
-    electrons = False
-    photons = True
-
-  if _mgr.is_open():
-    while _mgr.next_event():
-      showers = _mgr.get_data(larlite.data.kShower, "showerreco")
-      for shower in showers:
-
-        # Cut events that are crazy
-        if shower.OpeningAngle() > 1:
-          continue
-
-        if shower.Length() < 10:
-          continue
-
-        # Get the dE/dx vector:
-        dedx_v = shower.dEdx_v()
-        dqdx_v = shower.dQdx_v()
-
-        if dedx_v[1] > 10:
-          continue
+def birksCorrection(dqdx):
+    result = []
+    for x in dqdx_v:
+        result.append(larutil.LArProperties.GetME().BirksCorrection(x))
+    return result
 
 
-        if electrons:
-          electron_data.append(dedx_v[1])
-          electron_data_q.append(dqdx_v[1])
-        if photons:
-          photon_data.append(dedx_v[1])
-          photon_data_q.append(dqdx_v[1])
+if __name__ == '__main__':
+  print "Making dE/dx plots."
 
-print len(electron_data)
-print len(photon_data)
+  larutil.LArUtilManager.Reconfigure(larlite.geo.kArgoNeuT)
 
 
-# bins = np.linspace(0,7,25)
-
-fig, ax = plt.subplots()
-ax.hist(electron_data,alpha=0.5)
-ax.hist(photon_data,alpha=0.5)
-
-plt.title("dE/dx Separation of electrons and photons")
-plt.xlabel("dE/dx [MeV/cm]")
-
-fig, ax = plt.subplots()
-
-ax.hist(electron_data_q,alpha=0.5)
-ax.hist(photon_data_q,alpha=0.5)
-
-plt.title("dQ/dx Separation of electrons and photons")
-plt.xlabel("dQ/dx [???/cm]")
+  # Need the files to work on, which are basic root ttrees
+  # 
+  files = ["nu_electrons_anatree.root"]
 
 
-plt.show()
+  for f in files:
+    mydf= pd.DataFrame( root2array( f ) )
+
+    mydf['c_hitcharges'] *= mydf["c_lifetime_corr"]
+    mydf['c_hitpeaks'] *= mydf["c_lifetime_corr"]
+    mydf['i_hitcharges'] *= mydf["i_lifetime_corr"]
+    mydf['i_hitpeaks'] *= mydf["i_lifetime_corr"]
+
+    mydf['c_hitcharges_q'] = mydf['c_hitcharges'] * Q_AREA[1]
+    mydf['c_hitpeaks_q'] = mydf['c_hitpeaks'] * Q_AMP[1]
+    mydf['i_hitcharges_q'] = mydf['i_hitcharges'] * Q_AREA[0]
+    mydf['i_hitpeaks_q'] = mydf['i_hitpeaks'] * Q_AMP[0]
+    mydf['c_hitcharges_dq'] = mydf['c_hitcharges_q'] * E_AREA[1]
+    mydf['c_hitpeaks_dq'] = mydf['c_hitpeaks_q'] * E_AMP[1]
+    mydf['i_hitcharges_dq'] = mydf['i_hitcharges_q'] * E_AREA[0]
+    mydf['i_hitpeaks_dq'] = mydf['i_hitpeaks_q'] * E_AMP[0]
+    mydf['c_charge_dqdx'] = mydf['c_hitcharges_dq'] / mydf['c_pitch']
+    mydf['c_peak_dqdx'] = mydf['c_hitpeaks_dq'] / mydf['c_pitch']
+    mydf['i_charge_dqdx'] = mydf['i_hitcharges_dq'] / mydf['i_pitch']
+    mydf['i_peak_dqdx'] = mydf['i_hitpeaks_dq'] / mydf['c_pitch']
+
+    mydf['c_charge_dedx'] = mydf['c_charge_dqdx'].apply(boxCorrection)
+
+    for index, row in mydf.iterrows():
+        plt.plot( row['c_hitwires'], row['c_charge_dedx'])
+        plt.show()
+    # plt.show()
+    # a = mydf.loc[0]
+    # print type(a)
+    # print a
+    # a['q_cal_collection_hitcharges'] = a.collection_hitcharges * Q_AREA[1]
+    # print a
